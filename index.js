34ad174ec630e161395e8c9f0885e295511bc9e5
Nick <admiralflapplak@gmail.com>
2021-01-27 20:02:55 -0500

    please

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
-const port =process.env.PORT;
+const port = process.env.PORT;
 app.listen(port, () => {
     console.log('listening at 3000');
 });
